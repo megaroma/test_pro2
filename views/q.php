ab725f7db2c083a9299a4c73046b80c77e3d5593
Roman <darkromanovich@gmail.com>
2013-12-05 12:38:52 -0800

    fix view

--- views/q.php
+++ views/q.php
@@ -5,10 +5,10 @@
 
 <div data-bind="foreach: questions">
 <div class="Box">
-  <div data-bind="html: q_info"></div>
+  <div data-bind='if: mini() == 1'><div data-bind="html: q_info"></div></div>
   <b><font data-bind="html: q_text"></font></b>
   <font data-bind='if: mini() == 2'><font data-bind='text: answer'></font><button data-bind="click: maximise">&darr;</button></font>
-  <div data-bind='if: mini() == 1'  >
+  <div data-bind='if: mini() == 1'>
     <div data-bind='foreach: choices'>
     <input data-bind='checked: $data.checked,attr:{name: $data.up},value: $data.move,event:{change: $parent.add}' type='radio'  /><font data-bind='text: $data.text'></font><br>
     </div>
